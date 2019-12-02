67a3a1b37acb6d38198d42ee41e888458ec42033
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2019-12-02 19:54:48 +0100

    Add toggle on class 'open-active'

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const panels = document.querySelectorAll('.panel');
 
+// Toggle class to open after click
 function toggleOpen() {
     this.classList.toggle("open");
 }
 
-panels.forEach(panel => panel.addEventListener('click',
-    toggleOpen))
+// Toggle class to open-active after transition
+function toggleActive() {
+    this.classList.toggle("open-active")
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
